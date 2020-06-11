897c85bdea6abf1cda33179f5c68f64003bfb9f4
Trend20 <enockomondi305@gmail.com>
2020-06-11 14:27:09 -0400

    input values

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
  
 class Form extends Component {
     render() { 
         return (
             <form id="form" method="POST">
-                
+                <div className="form-group">
+                    <label>Username</label>
+                    <input 
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Username"
+                    id="username" />
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input 
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter Password"
+                    id="password" />
+                </div>
             </form>
         );
     }
